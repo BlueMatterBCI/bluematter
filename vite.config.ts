ea41f216a3fea6be4143fb07344910a1ca0e43bc
Taylor Foster <147962748+TayFost@users.noreply.github.com>
2026-03-27 00:24:56 -0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
 
-const base = "/";
+const base = "/bluematter/";
 
 export default defineConfig({
   base,
